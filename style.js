463d1ecc194080cf9e91b6427821f7ae38075c76
kelvinkip <kelvinkipla@gmail.com>
2022-03-14 11:42:38 +0300

    manage to add jquary though not working as expected

--- style.js
+++ style.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     $("#design").click(function(event){
         $("#image1").show();
-        $("#hide").show();
-        $("#image1").show()
+        $("#hide").hide()
+        // $("#hide").show();
+        // $("#image1").show()
 
     })
 })
@@ -10,31 +11,49 @@ $(document).ready(function(){
 $(document).ready(function(){
     $("#development").click(function(event){
         $("#image2").show();
-        $("#hi").show();
+        $("#hi").hide();
 
     })
 })
 
 $(document).ready(function(){
     $("#product").click(function(event){
-        $("#image3").toggle();
+        $("#image3").show();
         $("#plan").hide();
 
     })
 })
 
-// $(document).ready(function(){
-//     $("#design").mouseover(function(){
-//       $("#hide").show();
-//       // $("#hide").hided()
-//     }).mouseout(function(){
-//       $("#hide").hide();
-//       $("#design").show()
-//     });
-//   });
-let myForm=document.querySelector("#mail")
+$(document).ready(function(){
+    $("#design").mouseover(function(){
+      $("#hide").show();
+      // $("#hide").hided()
+    }).mouseout(function(){
+      $("#hide").hide();
+      $("#design").show()
+    });
+  });
+  $(document).ready(function(){
+    $("#design").mouseover(function(){
+      $("#hide").show();
+      // $("#hide").hided()
+    }).mouseout(function(){
+      $("#hide").hide();
+      $("#design").show()
+    });
+  });
+  $(document).ready(function(){
+    $("#design").mouseover(function(){
+      $("#hide").show();
+      // $("#hide").hided()
+    }).mouseout(function(){
+      $("#hide").hide();
+      $("#design").show()
+    });
+  });
+let myForm=document.querySelector("#email")
   myForm.addEventListener("submit", (e) => {
     e.preventDefault()
-    let jina = myForm.jina.value;
-    alert(jina + "" + " we have received your message.Thank you for reaching out to us." );
+    let myname = myForm.myname.value;
+    alert(myname + "" + " we have received your message.Thank you for reaching out to us." );
 })
