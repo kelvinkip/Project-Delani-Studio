4b2ce73996707acc77b7d30d2e073903d38c8beb
kelvinkip <kelvinkipla@gmail.com>
2022-03-16 12:17:17 +0300

    design what we do section

--- style.js
+++ style.js
@@ -1,66 +1,61 @@
-$(document).ready(function(){
-    $("#design").click(function(event){
-        $("#image1").show();
-        $("#designp").hide()
+  $(document).ready(function(){
 
-    })
-})
+    
+    $("#design").click(function() {
+        $("#design").hide();
+        $("#designp").show();
+    });
+    $("#designp").click(function() {
+        $("#designp").hide();
+        $("#design").show();
+    });
 
-$(document).ready(function(){
-    $("#development").click(function(event){
-        $("#image2").show();
+   
+    $("#development").click(function() {
+        $("#development").hide();
+        $("#develop").show();
+    });
+    $("#develop").click(function() {
         $("#develop").hide();
+        $("#development").show();
+    });
 
-    })
-})
 
-$(document).ready(function(){
-    $("#product").click(function(event){
-        $("#image3").show();
+    $("#product").click(function() {
+        $("#product").hide();
+        $("#manage").show();
+    });
+    $("#manage").click(function() {
         $("#manage").hide();
+        $("#product").show();
+    });
 
-    })
-})
+    
+    $("#product").click(function() {
+        $("#product").hide();
+        $("#manage").show();
+    });
+    $("#manage").click(function() {
+        $("#manage").hide();
+        $("#product").show();
+    });
 
-$(document).ready(function(){
-  $("designp").hide()
-    $("#design").mouseover(function(){
-      $("#designp").show();
-      $("#design").hide()
-    }).mouseout(function(){
-      $("#designp").hide();
-      $("#design").show()
-    });
-  });
-  $(document).ready(function(){
-    $("#development").mouseover(function(){
-      $("#develop").show();
-      $("#development").hide()
-    }).mouseout(function(){
-      $("#develop").hide();
-      $("#development").show()
-    });
-  });
-  $(document).ready(function(){
-    $("#product").mouseover(function(){
-      $("#manage").show();
-      $("#product").hide()
-    }).mouseout(function(){
-      $("#manage").hide();
-      $("#product").show()
-    });
-  });
+
+
+
+
+
 $("form#form").submit(function(event){
  var name = $("input#name").val();
  var email = $("input#email").val();
  var message = $("textarea#message").val();
 
  if ($("input#name").val() && $("input#email").val()){
- alert ("Hey " + name + ", we have received your wonderful message. Thank you for reaching out to Delani Studio, We shall get back to you ASAP!!");
+ alert ("Hey " + name + ", we have received your message. Thank you for contacting Delani Studio, We shall get back to you very soon thanks!!");
  }
  else {
-     alert("Please enter your name and email!");
+     alert("Kindly enter your name and email!");
  }
-   
+ 
 });
-
+  });
