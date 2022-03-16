57c0e0475b23a08befd58e1bdc1434c18ca20d66
kelvinkip <kelvinkipla@gmail.com>
2022-03-16 11:53:48 +0300

    manage to add alert in the contact section

--- style.js
+++ style.js
@@ -50,14 +50,7 @@ $(document).ready(function(){
       $("#product").show()
     });
   });
-// let myForm=document.querySelector("#email")
-//   myForm.addEventListener("submit", (e) => {
-//     e.preventDefault()
-//     let myname = myForm.myname.value;
-//     alert(myname + "" + " we have received your message.Thank you for reaching out to us." );
-// })
 $("form#form").submit(function(event){
-  // event.preventDefault();
  var name = $("input#name").val();
  var email = $("input#email").val();
  var message = $("textarea#message").val();
